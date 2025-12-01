8a1b2ad2466035b87c7c832959f4a04c7640b141
Alexandre Riou <hidden>
2025-12-02 00:24:36 +0100

    implent part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,19 +5,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let instructions = fs::read_to_string("input1.txt")?;
     let lines = instructions.lines();
     let mut pos: i32 = 50;
-    let mut counter: i32 = 0;
+    let mut counter_part1: i32 = 0;
+    let mut counter_part2: i32 = 0;
     for line in lines{
         let (dir, amount)= line.split_at(1);
         let amount: i32 = amount.parse().unwrap();
         if dir == "R" {
-            pos = (pos + amount) % 100 
+            counter_part2 += (pos + amount) / 100;
+            pos = (pos + amount) % 100;
         } else {
-            pos = (100 - (((100 - pos) + amount) % 100)) % 100
+            counter_part2 += (((100 - pos) % 100) + amount) / 100;
+            pos = (100 - (((100 - pos) + amount) % 100)) % 100;
         }
         if pos == 0 {
-            counter+=1;
+            counter_part1+=1;
         }
         }
-    println!("{counter}");
+    println!("Solution part 1: {counter_part1}\nSolution part 2: {counter_part2}");
     Ok(())
 }
