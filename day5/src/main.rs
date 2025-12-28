351e390a5af401861025916ce5e981f2ff0387bf
Alexandre Riou <hidden>
2025-12-28 16:06:01 +0100

    solve day 5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::error::Error;
+use std::cmp;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut counter_part1: i32 = 0;
@@ -22,5 +23,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
     println!("Solution part 1: {counter_part1}");
+
+    let exclusive_ranges = compute_exclusive_ranges(ranges);
+    let counter_part2 = exclusive_ranges.iter().fold(0, |acc, &e| acc + e.1 - e.0 + 1);
+    println!("Solution part 2: {counter_part2}");
     Ok(())
+}
+
+fn has_overlap(range1: (u64, u64), range2: (u64, u64)) -> bool {
+    (range1.0 <= range2.0 && range2.0 <= range1.1) || (range2.0 <= range1.0 && range1.0 <= range2.1)
+}
+
+fn compute_exclusive_ranges(ranges: Vec<(u64, u64)>) -> Vec<(u64, u64)> {
+    let mut exclusive_ranges: Vec<(u64, u64)> = Vec::new();
+    for range in ranges {
+        let mut future_exclusive_ranges: Vec<(u64, u64)> = Vec::new();
+        let mut overlapping_ranges: Vec<(u64, u64)> = Vec::new();
+        for exclusive_range in exclusive_ranges {
+            if has_overlap(range, exclusive_range) {
+                overlapping_ranges.push(exclusive_range);
+            } else {
+                future_exclusive_ranges.push(exclusive_range);
+            }
+        }
+        let mut start = range.0;
+        let mut end = range.1;
+        for to_merge in overlapping_ranges {
+            start = cmp::min(start, to_merge.0);
+            end = cmp::max(end, to_merge.1);
+        }
+        future_exclusive_ranges.push((start, end));
+        exclusive_ranges = future_exclusive_ranges
+    }
+    exclusive_ranges
 }
