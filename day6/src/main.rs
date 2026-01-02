f40abef42c6cb819bb99495ca004f4dbf0956686
Alexandre Riou <hidden>
2026-01-02 18:31:26 +0100

    implement day6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,7 +2,6 @@ use std::fs;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut counter_part1: u64 = 0;
     let file = fs::read_to_string("input.txt")?;
     let mut instructions: Vec<&str>= file.lines().collect();
     let operators: Vec<&str> = instructions.pop().unwrap()
@@ -23,14 +22,40 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         operands_t.push(column);
     }
-    
-    for i in 0..operators.len() {
-        if operators[i] == "*" {
-            counter_part1 += operands_t[i].iter().copied().reduce(|acc, e| acc * e).unwrap()
+
+    let instructions_chars: Vec<Vec<char>> = instructions.iter().map(|&line| line.chars().collect()).collect();
+    let mut column_operands: Vec<Vec<u64>> = Vec::new();
+    let mut buffer: Vec<u64> = Vec::new();
+    for x in 0..instructions_chars[0].len() {
+        let mut char_vec: Vec<char> = Vec::new();
+        for y in 0..instructions_chars.len() {
+            char_vec.push(instructions_chars[y][x]);
+        }
+        let operand_string: String = char_vec.into_iter().collect();
+        if operand_string.trim().is_empty() {
+            column_operands.push(buffer);
+            buffer = Vec::new();
         } else {
-            counter_part1 += operands_t[i].iter().copied().reduce(|acc, e| acc + e).unwrap()
+            buffer.push(operand_string.trim().parse::<u64>().unwrap());
         }
     }
-    println!("Solution part 1: {counter_part1}");
+    column_operands.push(buffer);
+
+    
+    let counter_part1 = compute_results(&operators, operands_t);
+    let counter_part2 = compute_results(&operators, column_operands);
+    println!("Solution part 1: {counter_part1}\nSolution part 2: {counter_part2}");
     Ok(())
+}
+
+fn compute_results(operators: &Vec<&str>, operands: Vec<Vec<u64>>) -> u64 {
+    let mut counter: u64 = 0;
+    for i in 0..operators.len() {
+        if operators[i] == "*" {
+            counter += operands[i].iter().copied().reduce(|acc, e| acc * e).unwrap()
+        } else {
+            counter += operands[i].iter().copied().reduce(|acc, e| acc + e).unwrap()
+        }
+    }
+    counter
 }
