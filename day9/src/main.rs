7251f702cfb92f666e7df5bb5a2048281a0d5955
Alexandre Riou <hidden>
2026-01-09 11:39:25 +0100

    failed implementation day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::cmp::max;
 use std::error::Error;
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 struct Tile {
     x: u32,
     y: u32
@@ -17,15 +17,22 @@ impl Tile {
 fn main() -> Result<(), Box<dyn Error>> {
     let instructions = fs::read_to_string("input.txt")?;
     let tiles: Vec<Tile> = instructions.lines().map(self::parse_tile).collect();
-    let mut max_area: u64 = 0;
+    let mut counter_part1: u64 = 0;
+    let mut counter_part2: u64 = 0;
+
     for i in 0..tiles.len() {
         for j in i+1..tiles.len() {
             let tile1 = &tiles[i];
             let tile2 = &tiles[j];
-            max_area = max(max_area, tile1.area_with(tile2))
+            let area = tile1.area_with(tile2);
+            counter_part1 = max(counter_part1, area);
+            if path_does_not_go_inside(tile1, tile2, &tiles) {
+                counter_part2 = max(counter_part2, area)
+            }
         }
     }
-    println!("Solution part 1: {max_area}");
+    println!("Solution part 1: {counter_part1}\nSolution part 2: {counter_part2}");
+    // 87130620 too low
     Ok(())
 }
 
@@ -35,4 +42,75 @@ fn parse_tile(line: &str) -> Tile {
         x: coords.next().unwrap().parse::<u32>().unwrap(),
         y: coords.next().unwrap().parse::<u32>().unwrap(),
     }
+}
+
+fn path_does_not_go_inside(corner1: &Tile, corner2: &Tile, path: &Vec<Tile>) -> bool {
+    let left: &Tile;
+    let right: &Tile;
+    if corner1.x <= corner2.x {
+        left = corner1;
+        right = corner2;
+    } else {
+        left = corner2;
+        right = corner1;
+    }
+
+    let top: &Tile;
+    let bottom: &Tile;
+    if corner1.y <= corner2.y {
+        top = corner1;
+        bottom = corner2;
+    } else {
+        top = corner2;
+        bottom = corner1;
+    }
+
+    for i in 0..path.len() {
+        let j = (i + 1) % path.len();
+        let tile1 = &path[i];
+        let tile2 = &path[j];
+
+        if tile1.x == tile2.x {
+            // vertical segment
+            if tile1.x > left.x && tile1.x < right.x {
+                let top_of_segment: &Tile;
+                let bottom_of_segment: &Tile;
+                if tile1.y > tile2.y {
+                    top_of_segment = tile2;
+                    bottom_of_segment = tile1;
+                }
+                else {
+                    top_of_segment = tile1;
+                    bottom_of_segment = tile2;
+                }
+                if top_of_segment.y < bottom.y && bottom_of_segment.y > top.y {
+                    return false;
+                }
+            }
+        } else {
+            // horizontal segment
+            if tile1.y > top.y && tile1.y < bottom.y {
+                let left_of_segment: &Tile;
+                let right_of_segment: &Tile;
+                if tile1.x > tile2.x {
+                    left_of_segment = tile2;
+                    right_of_segment = tile1;
+                }
+                else {
+                    left_of_segment = tile1;
+                    right_of_segment = tile2;
+                }
+                if left_of_segment.y < right.x && right_of_segment.x > left.x {
+                    return false;
+                }
+            }
+        }
+    }
+
+    true
+}
+
+
+fn is_adjacent(tile1: &Tile, tile2: &Tile) -> bool {
+    ((tile1.x as i32 - tile2.x as i32).abs() <= 1 && tile1.y == tile2.y) || ((tile1.y as i32 - tile2.y as i32).abs() <= 1 && tile1.x == tile2.x)
 }
